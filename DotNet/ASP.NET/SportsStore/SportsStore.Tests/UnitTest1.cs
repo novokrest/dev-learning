68e2437e104e9be507deb923e2fd3e4d26e61539
novokrest <novokrest013@gmail.com>
2016-07-25 23:46:41 +0300

    SportStore: beginning...

---
+++ DotNet/ASP.NET/SportsStore/SportsStore.Tests/UnitTest1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System.Collections.Generic;
+using SportsStore.WebUI.Controllers;
+using System.Linq;
+using System.Web.Mvc;
+using SportsStore.WebUI.Models;
+using SportsStore.WebUI.HtmlHelpers;
+
+namespace SportsStore.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CanPaginate()
+        {
+            //arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product { ProductID = 1, Name = "P1" },
+                new Product { ProductID = 2, Name = "P2" },
+                new Product { ProductID = 3, Name = "P3" },
+                new Product { ProductID = 4, Name = "P4" },
+                new Product { ProductID = 5, Name = "P5" },
+            }.AsQueryable());
+
+            var productController = new ProductController(mock.Object);
+            productController.PageSize = 3;
+
+            //act
+            var result = (ProductListViewModel)productController.List(2).Model;
+
+            //assert
+            Product[] productArray = result.Products.ToArray();
+            Assert.IsTrue(productArray.Length == 2);
+            Assert.AreEqual(productArray[0].Name, "P4");
+            Assert.AreEqual(productArray[1].Name, "P5");
+        }
+
+        [TestMethod]
+        public void CanGeneratePageLinks()
+        {
+            //arrange
+            HtmlHelper htmlHelper = null;
+
+            var paggingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+
+            Func<int, string> pageUrl = i => "Page" + i;
+
+            //act
+            MvcHtmlString result = htmlHelper.PageLinks(paggingInfo, pageUrl);
+
+            //assert
+            Assert.AreEqual(result.ToString(), 
+                @"<a href=""Page1"">1</a>" +
+                @"<a class=""selected"" href=""Page2"">2</a>" +
+                @"<a href=""Page3"">3</a>");
+        }
+
+        [TestMethod]
+        public void CanSendPaginationViewModel()
+        {
+            //arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product { ProductID = 1, Name = "P1" },
+                new Product { ProductID = 2, Name = "P2" },
+                new Product { ProductID = 3, Name = "P3" },
+                new Product { ProductID = 4, Name = "P4" },
+                new Product { ProductID = 5, Name = "P5" },
+            }.AsQueryable());
+
+            var productController = new ProductController(mock.Object);
+            productController.PageSize = 3;
+
+            //act
+            var result = (ProductListViewModel)productController.List(2).Model;
+
+            //assert
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.AreEqual(pageInfo.CurrentPage, 2);
+            Assert.AreEqual(pageInfo.ItemsPerPage, 3);
+            Assert.AreEqual(pageInfo.TotalItems, 5);
+            Assert.AreEqual(pageInfo.TotalPages, 2);
+
+        }
+    }
+}
---
+++ DotNet/ASP.NET/SportsStore/SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Ninject;
+using SportsStore.Domain.Abstract;
+using Moq;
+using SportsStore.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using SportsStore.Domain.Concrete;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private readonly IKernel _ninjectKernel;
+
+        public NinjectControllerFactory()
+        {
+            _ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null
+                ? null
+                : (IController)_ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBindings()
+        {
+            _ninjectKernel.Bind<IProductRepository>().To<EFProductRepository>();
+        }
+    }
+}
